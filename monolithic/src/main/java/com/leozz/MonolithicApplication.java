0802b2fa37ea9d5f0a984e2f013fd1e6670551f0
leo-zz <pylibing@qq.com>
2019-03-12 10:38:38 +0800

    Create project and packages

---
+++ monolithic/src/main/java/com/leozz/MonolithicApplication.java
@@ -0,0 +1,15 @@
+package com.leozz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: leo-zz
+ * @Date: 2019/3/12 10:14
+ */
+@SpringBootApplication
+public class MonolithicApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MonolithicApplication.class, args);
+    }
+}
