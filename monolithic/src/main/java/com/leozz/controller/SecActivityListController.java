90607eebaaade97a2d193a79ceeccddec9b1f5ea
leo-zz <pylibing@qq.com>
2019-03-16 18:02:55 +0800

    update controller code

--- monolithic/src/main/java/com/leozz/controller/SecActivityListController.java
+++ monolithic/src/main/java/com/leozz/controller/SecActivityListController.java
@@ -1,6 +1,7 @@
 package com.leozz.controller;
 
 import com.leozz.dto.PreSubmitOrderDTO;
+import com.leozz.dto.ResultDTO;
 import com.leozz.dto.SecActivityDTO;
 import com.leozz.service.SecActivityService;
 import com.leozz.service.SecOrderService;
@@ -8,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 /**
@@ -25,11 +28,12 @@ public class SecActivityListController {
     private SecOrderService secOrderService;
     /**
      * 获取当前秒杀活动列表
-     * @param userId userId 用户id
      * @return 秒杀活动列表页需要的数据
      */
     @RequestMapping("/list")
-    public List<SecActivityDTO> getSecActivityList(Long userId){
+    public List<SecActivityDTO> getSecActivityList(HttpServletRequest request){
+        HttpSession session = request.getSession();
+        Long userId = (Long)session.getAttribute("userId");
         //根据userId获取秒杀活动列表，是否要通过mybatis关联查询？
         return secActivityService.getSecActivityList(userId);
     }
@@ -37,20 +41,23 @@ public class SecActivityListController {
     /**
      * 点击按钮，参与秒杀活动，参与成功则跳转到预下单页面。
      * @param secActivityId
-     * @param userId
      * @return 下单页需要的数据
      */
     @RequestMapping("/partake")
-    public PreSubmitOrderDTO partakeSecActivity(Long secActivityId, Long userId){
+    public PreSubmitOrderDTO partakeSecActivity(HttpServletRequest request,Long secActivityId){
+        HttpSession session = request.getSession();
+        Long userId = (Long)session.getAttribute("userId");
 
         PreSubmitOrderDTO preSubmitOrderDTO =null;
         //尝试参与秒杀活动
-        boolean b = secActivityService.partakeSecActivity(secActivityId, userId);
-        if(b){
+        ResultDTO resultDTO = secActivityService.partakeSecActivity(secActivityId, userId);
+        if(resultDTO.isResult()){
+            //将用户当前参与的秒杀活动放入session
+            session.setAttribute("secActivityId",secActivityId);
             //获取预下单页面的信息
             preSubmitOrderDTO = secOrderService.preSubmitOrder(secActivityId, userId);
         }else{
-            preSubmitOrderDTO = new PreSubmitOrderDTO(false);
+            preSubmitOrderDTO = new PreSubmitOrderDTO(false,resultDTO.getMsg());
         }
         return preSubmitOrderDTO;
     }
--- monolithic/src/main/java/com/leozz/controller/SecOrderController.java
+++ monolithic/src/main/java/com/leozz/controller/SecOrderController.java
@@ -1,10 +1,14 @@
 package com.leozz.controller;
 
+import com.leozz.dto.ResultDTO;
+import com.leozz.dto.SubmitDTO;
 import com.leozz.entity.SecOrder;
 import com.leozz.service.SecOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @Author: leo-zz
@@ -19,13 +23,9 @@ public class SecOrderController {
 
     //提交订单，然后跳转到付款页面。参数需要等到具体写代码时才能确定
     @RequestMapping("/submit")
-    public boolean submitOrder(SecOrder order, long couponIDs) {
-        boolean result = false;
-        int i = secOrderService.submitOrder(order, couponIDs);
-        if (i != -1) {
-            result = true;
-        }
-        return result;
+    public ResultDTO submitOrder(SubmitDTO submitDTO) {
+
+        return secOrderService.submitOrder(submitDTO);
     }
 
 
