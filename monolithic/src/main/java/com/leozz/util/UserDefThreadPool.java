6c01a7de2bfc1756f1aa6d22b02d10873476df4d
leo-zz <pylibing@qq.com>
2019-03-31 17:03:43 +0800

    Add threadpool name

--- monolithic/src/main/java/com/leozz/util/UserDefThreadPool.java
+++ monolithic/src/main/java/com/leozz/util/UserDefThreadPool.java
@@ -1,13 +1,21 @@
 package com.leozz.util;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @Author: leo-zz
  * @Date: 2019/3/15 10:42
  */
 public class UserDefThreadPool {
-    public  static ExecutorService jobExecutor = Executors.newFixedThreadPool(10);
+    public  static ExecutorService jobExecutor =new ThreadPoolExecutor(5, 5,
+            1000, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>(),
+            new ThreadFactory() {
+        private AtomicInteger num=new AtomicInteger();
 
+        @Override
+        public Thread newThread(Runnable r) {
+            return new Thread(r,"jobExecutor-"+num.incrementAndGet()+"pool");
+        }
+    });
 }
